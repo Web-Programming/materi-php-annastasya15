7bf949e64b5919bcc82e5dcb7d2fff83fc6d8f46
YukiharaEiji <michaelsunmingce24@gmail.com>
2025-04-29 20:37:14 +0700

    commit  belajar view dan route

---
+++ latihan/resources/views/beranda.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>welcome to beranda aca uye</h1>
+    <h2>Hallo {{ $name }}</h2>
+    <h2>Your Email is {{ $email }}</h2>
+    <h2>Your addres is {{ $alamat }}</h2>
+</div>
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('beranda', function () {
+    return view('beranda',
+    [
+        'name' => 'Aca',
+        'email' => 'apriliatasya1504@gmail.com',
+        'alamat' => 'jakarta'
+    ]
+);
+});
+Route::get('/berita/{id}/{judul}',function ($id, $judul){
+    return view('berita', ['id' => $id, 'judul' => $judul]);
+});
